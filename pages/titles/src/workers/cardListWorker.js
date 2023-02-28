9b879ef5eb078f6356100dc7f42e81889fd17b1e
gabriel <gabriel.pereira@ceuma.br>
2023-02-28 09:09:43 -0300

    feat: adicionando worker para processamento de dados em segundo plano na pesquisa

--- pages/titles/src/workers/cardListWorker.js
+++ pages/titles/src/workers/cardListWorker.js
@@ -1,3 +1,14 @@
 onmessage = ({ data }) => {
-  console.log('data', data);
+  let counter = 0
+  console.log('activating blocking operation...', data.maxItems)
+  console.time('blocking-op')
+  // blocking function
+  // 1e5 = 100.000
+  for (; counter < data.maxItems; counter++) console.log('.')
+  console.timeEnd('blocking-op')
+
+  postMessage({
+    response: 'ok',
+    data: counter
+  })
 }
